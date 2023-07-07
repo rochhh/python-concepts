1d4f86ae332f0acd0f6a721bdb74d3301b35429c
Roch <rochishnu7@gmail.com>
2023-07-07 15:28:37 +0530

    single_num solution

---
+++ dsa_arrays/single_num.py
@@ -0,0 +1,16 @@
+def solution(nums):
+    arr = [0]*len(nums)
+    # if more than a single number missing 
+    # missing_list = [] # if more than a single number missing 
+    for n in nums:
+        arr[n] += 1 
+
+    for i , _ in enumerate(arr):
+        if arr[i] == 1:
+            return i 
+            # missing_list.append(i) # if more than a single number missing 
+    # return missing_list # if more than a single number missing 
+    
+nums = [1,2,3,4,5,6,6,5,2,1]
+
+print(solution(nums))
