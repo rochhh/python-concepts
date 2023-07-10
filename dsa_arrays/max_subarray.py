fa47bceb41bab462c7c62ed7d0a6a2c6ba8f7db2
Roch <rochishnu7@gmail.com>
2023-07-10 10:12:08 +0530

    max subarray , kadane's algo

---
+++ dsa_arrays/max_subarray.py
@@ -0,0 +1,18 @@
+def solution(nums):
+    curr_max = 0
+    fin = float('-inf')
+
+    for num in nums:
+        curr_max = curr_max + num
+
+        if curr_max > fin:
+            fin = curr_max
+
+        if curr_max < 0:
+            curr_max = 0
+    return fin 
+
+
+arr = [-2,1,-3,4,-1,2,1,-5,4]
+
+print(solution(arr))
