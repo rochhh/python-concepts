2b3729bf75834553e6be59b794c480b135e2a276
Roch <rochishnu7@gmail.com>
2023-07-10 23:06:06 +0530

    best time to buy and sell stock 1

---
+++ dsa_arrays/stock1.py
@@ -0,0 +1,24 @@
+def solution(nums):
+    buy = 0
+    sell = 1
+    fin = 0
+
+    while sell < len(nums):
+        
+        curr = nums[sell] - nums[buy]
+        
+        if nums[buy] < nums[sell]:
+            fin = max( curr , fin )
+            sell += 1
+        else:
+            buy = sell
+            sell += 1
+    return fin 
+        
+
+
+
+arr = [7,6,5,3,6,4]
+
+
+print(solution(arr))
