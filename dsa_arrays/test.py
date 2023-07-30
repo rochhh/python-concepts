59c31977b29ed4e1124803bd270e65913412e811
Roch <rochishnu7@gmail.com>
2023-07-31 00:04:27 +0530

    max_subarray question fucked

--- dsa_arrays/test.py
+++ dsa_arrays/test.py
@@ -17,7 +17,7 @@ rev arr
 anagram 
 max , min no in an array 
 chocolate distribution 
-maximum subarray 
+maximum subarray ( !!!! got wrong after prac again  !!! )
 stock ~( little gray )
 nums disappearing in an array ~( little gray )
 first last elem in a sorted array
@@ -26,9 +26,31 @@ first last elem in a sorted array
 
 
 def soln(nums):
-    pass
+    i = 0
+    j = 1
+    curr_sum = nums[i]
+    max_sum = -1000000
+
+    if len(nums) == 1:
+        return nums[0]
+    
+    if len(nums) == 0 :
+        return 
+
+    while j < len(nums):
+
+        curr_sum = curr_sum + nums[j]
+        max_sum = max(max_sum,curr_sum)
+
+        if curr_sum < 0:
+            curr_sum = 0
+            i+=1
+
+        j += 1
+
+    return max_sum
 
 
-nums= [4,5,6,7,0,1,2]
-target = 0
-print(soln(nums,target))
+nums= [-2,1,-3,4,-1,2,1,-5,4]
+
+print(soln(nums))
