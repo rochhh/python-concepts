0e30aa8aa812fb2c2d3db2012fe6068876b7d3bb
Roch <rochishnu7@gmail.com>
2023-09-06 13:29:09 +0530

    understanding of tail and head recursion

--- recursion/print.py
+++ recursion/print.py
@@ -2,7 +2,13 @@ def func(n):
 
     if n > 0:
         print(n)
-        return func(n-1)
-    # return 
+        func(n-1)
+        print("i will be called after 3 2 1 ..." , n)
+
+    else:
+        print("outside the if block and n -> " ,n)
+        # n = n + 1 does not execute
+    
+    return 
 
 print(func(3))
